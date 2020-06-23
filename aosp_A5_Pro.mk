01dc128e0e0db6fdd151205f915aed71af5f1dd4
jmpfbmx <jmpf.bmx@gmail.com>
2020-06-23 15:50:09 +0200

    A5_Pro: EvoX-ify

--- aosp_A5_Pro.mk
+++ aosp_A5_Pro.mk
@@ -5,9 +5,6 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-# Release name
-PRODUCT_RELEASE_NAME := breeze
-
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
@@ -16,10 +13,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from UMIDIGI A5 Pro device
 $(call inherit-product, device/UMIDIGI/A5_Pro/device.mk)
 
-# Inherit some common PixelExperience stuff.
+# Inherit some common EvolutionX stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_GAPPS_ARCH := arm64
-$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Release name
+PRODUCT_RELEASE_NAME := breeze
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := aosp_A5_Pro
@@ -27,6 +27,15 @@ PRODUCT_DEVICE := A5_Pro
 PRODUCT_BRAND := UMIDIGI
 PRODUCT_MODEL := A5_Pro
 PRODUCT_MANUFACTURER := UMIDIGI
+
+PRODUCT_GMS_CLIENTID_BASE := android-umidigi
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="A5_Pro" \
+    PRODUCT_NAME="A5_Pro" \
+    PRIVATE_BUILD_DESC="A5_Pro-user 9 PPR1.180610.011 1559618636 release-keys"
+
+BUILD_FINGERPRINT := "google/flame/flame:10/QQ2A.200501.001.B2/6352890:user/release-keys"
 
 # Product characteristics
 PRODUCT_AAPT_CONFIG := xxhdpi
@@ -38,16 +47,4 @@ TARGET_SCREEN_WIDTH := 1080
 
 # Ringtone
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.ringtone=The_big_adventure.ogg \
-
-# Build info
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE="A5_Pro" \
-    PRODUCT_NAME="A5_Pro" \
-    PRIVATE_BUILD_DESC="A5_Pro-user 9 PPR1.180610.011 1559618636 release-keys"
-
-# Fingerprint
-BUILD_FINGERPRINT := "google/flame/flame:10/QQ2A.200501.001.B2/6352890:user/release-keys"
-
-# GMS as per stock
-PRODUCT_GMS_CLIENTID_BASE := android-umidigi
+    ro.config.ringtone=The_big_adventure.ogg
